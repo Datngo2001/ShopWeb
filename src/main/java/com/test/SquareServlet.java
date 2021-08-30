28fc4dd56bae85df14bfb1fd5b21bd52e1095879
HaDius <haiduongit@protonmail.com>
2021-08-31 00:14:23 +0700

    add Dockerfile, docker-compose, dockerignore

--- src/main/java/com/test/SquareServlet.java
+++ src/main/java/com/test/SquareServlet.java
@@ -10,20 +10,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 public class SquareServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-		
+
 		HttpSession session = req.getSession();
-		
-		//int c = Integer.parseInt(req.getParameter("c"));
-		
+
+		// int c = Integer.parseInt(req.getParameter("c"));
+
 		int c = (int) session.getAttribute("c");
-		
+
 		c = c * c;
-		
+
 		PrintWriter out = res.getWriter();
-		
+
 		out.println("Result is: " + c);
 
 	}
