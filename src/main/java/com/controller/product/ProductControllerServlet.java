a5c47c5078e888fc37b377edd35c46aa9cc70ec7
ngocthien2306 <75513398+ngocthien2306@users.noreply.github.com>
2021-09-13 21:33:12 +0700

    display product by servlet

---
+++ src/main/java/com/controller/product/ProductControllerServlet.java
@@ -0,0 +1,59 @@
+package com.controller.product;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import com.helperClass.ProductDbUtil;
+import com.model.Product;
+
+@WebServlet("/ProductControllerServlet")
+public class ProductControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private ProductDbUtil productDb;
+    @Resource(name="jdbc/shop")
+    private DataSource dataSource;
+    
+    
+    @Override
+	public void init() throws ServletException {
+		super.init();
+		try {
+			productDb = new ProductDbUtil(dataSource);
+		}
+		catch(Exception ex) {
+			throw new ServletException(ex);
+		}
+		
+	}
+	public ProductControllerServlet() {
+        super();
+        
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			listProduct(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	private void listProduct(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		List<Product> product = productDb.getProducts();
+		request.setAttribute("list_product", product);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("home.jsp");
+		dispatcher.forward(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/java/com/model/Product.java
@@ -0,0 +1,46 @@
+package com.model;
+
+public class Product {
+	public int id;
+	public String name;
+	public String description;
+	public int price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public Product(String name, String description, int price) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+	public Product(int id, String name, String description, int price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+	
+}
